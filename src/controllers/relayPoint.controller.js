5be454ab8c4f8a27e1b8353a72cbafbe77ef06d1
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-21 12:24:15 +0100

    chore: improve controller

--- src/controllers/relayPoint.controller.js
+++ src/controllers/relayPoint.controller.js
@@ -8,10 +8,28 @@ export const findRelay = async (req, res) => {
   const privateK = 'PrivateK'
   const hashString = enseigne + pays + city + privateK
   const hash = CryptoJS.MD5(hashString).toString(CryptoJS.enc.Hex).toUpperCase()
-  const soapString = `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><WSI2_RecherchePointRelais xmlns="http://www.mondialrelay.fr/webservice/"><Enseigne>${enseigne}Y</Enseigne><Pays>${pays}</Paysh><Ville>${city}</Ville><Security>${hash}</Security></WSI2_RecherchePointRelais></soap:Body></soap:Envelope>`
-
-  const response = await axios.post('https://api.mondialrelay.com/Web_Services.asmx/WSI2_PointRelais', soapString, { headers: { "Content-Type": "text/xml; charset=utf-8" } })
-
+  const soapString = `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <WSI3_PointRelais_Recherche xmlns="http://www.mondialrelay.fr/webservice/">
+        <Enseigne>${enseigne}</Enseigne>
+        <Pays>${pays}</Pays>
+        <Ville>${city}</Ville>
+        <Security>${hash}</Security>
+        </WSI3_PointRelais_Recherche>
+      </soap:Body>
+    </soap:Envelope>
+  `
+  const response = await axios.post('https://api.mondialrelay.com/Web_Services.asmx', soapString,
+    {
+      headers: {
+        "Content-Type": "text/xml; charssset=utf-8",
+        SOAPAction:
+          "http://www.mondialrelay.fr/webservice/WSI3_PointRelais_Recherche",
+      },
+    }
+  )
+  
   try {
     res.send({ ok: true, msg: response.data, city: city })
   } catch (error) {
