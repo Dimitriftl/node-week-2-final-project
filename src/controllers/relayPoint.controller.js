fb1e2f04ea1985e6cb6e325cc65b19f6e674fe83
tristan-greffe <tristan.greffe@hotmail.com>
2023-12-21 16:36:25 +0100

    chore: improve controller

--- src/controllers/relayPoint.controller.js
+++ src/controllers/relayPoint.controller.js
@@ -2,6 +2,29 @@ import axios from 'axios'
 import CryptoJS from 'crypto-js'
 import { xml2json } from '@codask/xml2json'
 
+function formatOpeningHours (hoursArray) {
+  if (hoursArray[0] === '0001' && hoursArray[1] === '2359' && hoursArray[2] === '0000' && hoursArray[3] === '0000') return 'h24'
+  if (hoursArray[0] === '0000' && hoursArray[1] === '0000' && hoursArray[2] === '0000' && hoursArray[3] === '0000') return 'closed'
+
+  const formatTime = time => {
+    const hours = time.substring(0, 2)
+    const minutes = time.substring(2)
+    return `${hours}h${minutes !== '00' ? `${minutes}` : ''}`
+  }
+  const openingHours = hoursArray.map(formatTime)
+  
+  const timeRanges = []
+  for (let i = 0; i < openingHours.length; i += 2) {
+    const start = openingHours[i]
+    const end = openingHours[i + 1]
+    if (start !== '0000' || end !== '0000') {
+      timeRanges.push(`${start} - ${end}`)
+    }
+  }
+  if (timeRanges[1] === '00h - 00h') return timeRanges[0]
+  return timeRanges.join(' | ')
+}
+
 export const findRelay = async (req, res) => {
   const city = req.query.city
   const enseigne = 'BDTEST13'
@@ -24,7 +47,30 @@ export const findRelay = async (req, res) => {
 
   try {
     const response = await axios.post('https://api.mondialrelay.com/Web_Services.asmx', soapString, { headers: { 'Content-Type': 'text/xml; charssset=utf-8', SOAPAction: 'http://www.mondialrelay.fr/webservice/WSI3_PointRelais_Recherche' }})
-    res.send({ ok: true, msg: xml2json(`${response.data}`), city: city })
+    const jsonResponse = xml2json(`${response.data}`)['soap:Envelope']['soap:Body']['WSI3_PointRelais_RechercheResponse']['WSI3_PointRelais_RechercheResult']['PointsRelais']['PointRelais_Details']
+    const msg = jsonResponse.map(item => {
+      return {
+        id: item.Num,
+        name: item.LgAdr1,
+        address: `${item.LgAdr3}, ${item.CP}, ${item.Ville}`,
+        country: item.Pays,
+        location: {
+          lat: item.Latitude,
+          long: item.Longitude
+        },
+        openingHours: {
+          monday: formatOpeningHours(item.Horaires_Lundi.string),
+          tuesday: formatOpeningHours(item.Horaires_Mardi.string),
+          wednesday: formatOpeningHours(item.Horaires_Mercredi.string),
+          thursday: formatOpeningHours(item.Horaires_Jeudi.string),
+          friday: formatOpeningHours(item.Horaires_Vendredi.string),
+          saturday: formatOpeningHours(item.Horaires_Samedi.string),
+          sunday: formatOpeningHours(item.Horaires_Dimanche.string)
+        },
+        imageUrl: item.URL_Photo
+      }
+    })
+    res.send({ ok: true, msg, city: city })
   } catch (error) {
     res.status(500).send({ ok: false, msg: 'Something went wrong', error: error.message })
   }
